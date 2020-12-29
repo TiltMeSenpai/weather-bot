57cc610499912905eef9e2f864f75be1bca27538
Josh Koike <jk@tilting.me>
2020-12-29 10:54:16 -0800

    Fix for incorrect ping response

--- index.js
+++ index.js
@@ -33,20 +33,24 @@ async function handlePayload(payload){
     console.log(`Got payload type: ${payload.type}`)
     switch(payload.type){
         case 1: return {type: 1}
-        case 2: 
-            const args = {}
-            payload.data.options.forEach(option => {
-                args[option.name] = option.value
-            });
-            const resp = commands[payload.data.name](args)
-            console.log(`Payload Args: ${args}`)
-            console.log(`Payload data: ${JSON.stringify(payload.data)}`)
-            if(typeof resp == "object"){
-                return resp
-            }
-            else
-                return {
-                    content: resp
-                }
+        case 2:
+            return {type: 4, data: await handleInteraction(payload)}
     }
+}
+
+async function handleInteraction(payload) {
+    const args = {}
+    payload.data.options.forEach(option => {
+        args[option.name] = option.value
+    });
+    const resp = commands[payload.data.name](args)
+    console.log(`Payload Args: ${args}`)
+    console.log(`Payload data: ${JSON.stringify(payload.data)}`)
+    if (typeof resp == "object") {
+        return resp
+    }
+    else
+        return {
+            content: resp
+        }
 }
