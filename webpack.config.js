d6934d5530b1ddf5c7b9858eb35344b63f85a322
Josh Koike <jk@tilting.me>
2020-12-31 13:09:59 -0800

    Remove nonfunctional source map

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ module.exports = {
     context: __dirname,
     target: "webworker",
     entry: path.resolve(__dirname, "./index.js"),
-    devtool: "source-map",
     plugins: [
         new WasmPackPlugin({
             crateDirectory: path.resolve(__dirname, "./crypto")
